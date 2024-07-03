074114110ed73998ca985da90757d180dae07dab
Patricio Leiva Ferrer <pleiva1138gmail.com>
2024-07-03 16:56:50 -0400

    Crud genero juego funcional

--- ReviewRealm/forms.py
+++ ReviewRealm/forms.py
@@ -6,7 +6,8 @@ from django.forms import ModelForm
 class GeneroJuegoForm(ModelForm):
     class Meta:
         model = Genero_Juego
-        fields = "__all__"
+        fields = ['nombre_genero']
+        labels = {'nombre_genero':'Nombre género'}
 
 # Se importa la clase Genero_Juego y esta hereda de ModelForm en la creación de la clase
 # La subclase Meta se encarga de indicar en qué modelo están lo datos y que campos se mostrarán "__all__" para todos
